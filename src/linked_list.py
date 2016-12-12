4f4647c902dd1280de6efc885ffaa9d0ee6d3a50
Colin Lamont <colinlamont@gmail.com>
2016-12-12 13:55:51 -0800

    added push function

--- src/linked_list.py
+++ src/linked_list.py
@@ -9,3 +9,8 @@ class LinkedList(object):
     def __init__(self, list):
         """Instantiate LinkedList class."""
         self.list = list
+
+    def push(self, val):
+        """Insert value to head of the list."""
+        self.list.insert(0, val)
+        return self.list
