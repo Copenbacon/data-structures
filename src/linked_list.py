8501db073a82996f555507f738a704195dd82664
copenbacon <sclary50@gmail.com>
2016-12-12 13:37:26 -0800

    LinkedList instantiates and new test for LinkedList.push()

---
+++ src/linked_list.py
@@ -0,0 +1,11 @@
+"""The Linked List Module Creates a LinkedList."""
+
+
+class LinkedList(object):
+    """Defining class of LinkedList."""
+
+    color = 'red'
+
+    def __init__(self, list):
+        """Instantiate LinkedList class."""
+        self.list = list
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -1,11 +1,23 @@
 """This tests the linked_list module."""
 import pytest
 
-SOME_TABLE = []
+INSTANTIATION_TABLE = ["new_linked_list", "[0, 1, 2]", "red"],
+
+PUSH_TABLE = ["new_linked_list", [0, 1, 2], 3, [3, 0, 1, 2]]
 
 
-@pytest.mark.parametrize("list, result", SOME_TABLE)
-def test_linkedlist(list, result):
+@pytest.mark.parametrize("list_name, nlist, result", INSTANTIATION_TABLE)
+def test_linkedlist(list_name, nlist, result):
     """Test instantiationg LinkedList class."""
     from linked_list import LinkedList
-    assert LinkedList(list) == result
+    list_name = LinkedList(nlist)
+    assert list_name.color == 'red'
+
+
+@pytest.mark.parametrize("list_name, nlist, value, result", PUSH_TABLE)
+def test_push(list_name, nlist, value, result):
+    """Test LinkedList push method."""
+    from linked_list import LinkedList
+    list_name = LinkedList(nlist)
+    list_name.push(value)
+    assert list_name.list == result
