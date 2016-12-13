07647d9b4c86ce8772a8a4655f9160687bb7d478
copenbacon <sclary50@gmail.com>
2016-12-13 15:07:17 -0800

    sending over setup and updated gitignore

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""Setupup.py for data-structures repo."""
+
+from setuptools import setup
+
+setup(
+    name="data-structures",
+    description="Implementation of a Linked List.",
+    version=0.1,
+    author="Colin Lamont, Conor Clary",
+    author_email="sclary50@gmail.com, colinLamont@gmail.com",
+    license="MIT",
+    package_dir={'': 'src'},
+    py_modules=['data-structures'],
+    install_requires=['ipython'],
+    extras_require={
+        "test": ['tox', 'pytest', 'pytest-watch', 'pytest-cov']
+    },
+)
