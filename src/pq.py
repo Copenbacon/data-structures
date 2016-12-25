1144d62ac4a62ec2bf83ac0deabf319a23fc1361
copenbacon <sclary50@gmail.com>
2016-12-24 16:42:12 -0800

    Priority Q and tests finished.

--- src/pq.py
+++ src/pq.py
@@ -1,32 +1,44 @@
 """Implement a priority queue."""
+import sys
 
-
-from math import inf
+if sys.version_info[0] == 3:
+    from math import inf
+else:
+    inf = float("inf")
 
 
 class PikaQ(object):
-    """The Priority Queue Class."""
+    """The Priority Queue Class.
+
+    def __init__(vals=[]): Initialize a priority queue, and insert any values passed in into the Priority Queue. Must pass a list of items. To pass
+    def insert(item, pri=float(-inf)): Insert an item into the Priority Queue, if no priority specified, -inf will be the priority,
+    def pop(): Remove a value from the priority queue.
+    def peek(): Look at next item to be popped in the Priority Queue.
+
+
+    """
 
     def __init__(self, vals=[]):
-        """Initialize a priority que."""
+        """Initialize a priority queue."""
         self.pq = {}
-        if hasattr(vals, "__iter__"):
-            try:
-                for val, pri in vals:
-                    self.insert(val, pri)
-            except TypeError:
-                for val in vals:
-                    self.insert(val)
-            except ValueError:
-                print("Too many values inserted in one of your lists")
-        elif not hasattr(type(vals, "__iter__")):
+        if not hasattr(vals, "__iter__"):
             self.insert(vals)
-        else:
-            raise ValueError("Need to submit either a single value, a list of values, or a list of values tupled/listed with priorities.")
+        elif hasattr(vals, "__iter__"):
+            if any(isinstance(item, dict) for item in vals):
+                self.insert(vals)
+            else:
+                try:
+                    for val, pri in vals:
+                        self.insert(val, pri)
+                except TypeError:
+                    for val in vals:
+                        self.insert(val)
+                except ValueError:
+                    raise ValueError("Submitting a string? Put it in a list or tuple first, and give it a priority! Otherwise it will be placed in the highest priority group.")
 
-    def insert(self, item, pri=float(-inf)):
-        """Insert."""
-        item = (item, pri)
+    def insert(self, val, pri=float(-inf)):
+        """Insert an item into the Priority Queue."""
+        item = (val, pri)
         if type(item[1]) is str or hasattr(type(item[1]), "__iter__") or type(item[1]) is bool:
             raise TypeError("Priority must be a number.")
         if item[1] in self.pq:
@@ -35,14 +47,18 @@ class PikaQ(object):
             self.pq[item[1]] = [item[0]]
 
     def pop(self):
-        """Poppin."""
+        """Remove a value from the prioritya queue."""
+        if len(self.pq) == 0:
+            raise IndexError("Empty PikaQ is un-poppable.")
         poppin_key = min(self.pq.keys())
         poppin_list = self.pq[poppin_key]
         self.pq[poppin_key].remove(poppin_list[0])
-        if len(self.pq) == 0:
-            raise IndexError("Empty PikaQ is un-poppable.")
         if len(poppin_list) == 0:
             self.pq.pop(poppin_key)
 
     def peek(self):
-        """Peekin."""
+        """Look at next item to be popped in the Priority Queue."""
+        if len(self.pq) > 0:
+            return str(min(self.pq)) + ": " + str(self.pq[min(self.pq)][0])
+        else:
+            return "Nothing, Priority Q is empty!"
---
+++ src/test_pq.py
@@ -0,0 +1,100 @@
+"""Test the priorityQ module, pq.py."""
+import pytest
+import sys
+
+if sys.version_info[0] == 3:
+    from math import inf
+else:
+    inf = float("inf")
+
+INIT_TABLE = [
+    [[(1, 0), (3, 4), [5, 6], [7, 8]], {0: [1], 4: [3], 6: [5], 8: [7]}],
+    [[1], {-inf: [1]}],
+    [1, {-inf: [1]}],
+    [[{1: 1}], {-inf: [[{1: 1}]]}],
+    [[({1: 1}, 1)], {1: [{1: 1}]}],
+]
+
+POP_TABLE = [
+    [[(1, 0), (3, 4), [5, 6], [7, 8], (3, 0)], 4],
+    [[1], 0],
+    [1, 0],
+]
+
+PEEK_TABLE = [
+    [[(1, 0), (3, 4), [5, 6], [7, 8], (3, 0)], '0: 1'],
+    [[1, 2, 3, 4, 5], '-inf: 1'],
+]
+
+
+@pytest.fixture
+def new_empty_priority_q():
+    """Empty PikaQ fixture."""
+    from pq import PikaQ
+    return PikaQ()
+
+
+def test_empty_initialization(new_empty_priority_q):
+    """Test that the empty priority queue exists."""
+    assert len(new_empty_priority_q.pq) == 0
+
+
+@pytest.mark.parametrize("vals, result", INIT_TABLE)
+def test_initialization_with_values(vals, result):
+    """Test that a PQ can be initialized with values."""
+    from pq import PikaQ
+    new_pq = PikaQ(vals)
+    assert new_pq.pq == result
+
+if sys.version_info[0] == 3:
+    def test_value_error():
+        """Raise a value error when initializing with just a string."""
+        from pq import PikaQ
+        with pytest.raises(ValueError):
+            new_pq = PikaQ("Some string right here.")
+
+
+def test_insert_actually_inserts(new_empty_priority_q):
+    """Inserting a single value should create a -inf node, which is the base and highest priority."""
+    new_empty_priority_q.insert(1)
+    assert new_empty_priority_q.pq == {-inf: [1]}
+
+
+def test_insert_2_vals_actually_inserts(new_empty_priority_q):
+    """Inserting two args should create a node with the second arg as a new priority."""
+    new_empty_priority_q.insert(1, 2)
+    assert new_empty_priority_q.pq == {2: [1]}
+
+
+def test_insert_bool_raise_error(new_empty_priority_q):
+    """Inserting a boolean as a priority should raise a TypeError."""
+    with pytest.raises(TypeError):
+        new_empty_priority_q.insert(2, True)
+
+
+@pytest.mark.parametrize("vals, result", POP_TABLE)
+def test_popping_removes_the_highest_priority_item(vals, result):
+    """Test that a PQ can be initialized with values."""
+    from pq import PikaQ
+    new_pq = PikaQ(vals)
+    new_pq.pop()
+    assert len(new_pq.pq) == result
+
+
+def test_popping_empty_raises_error(new_empty_priority_q):
+    """Test that an error is raised and handled when popping an empty Priority Q."""
+    with pytest.raises(IndexError):
+        new_empty_priority_q.pop()
+
+
+@pytest.mark.parametrize("vals, result", PEEK_TABLE)
+def test_peeking_reveals_the_highest_priority_item(vals, result):
+    """Test that peek() reveals the next highest priority item."""
+    from pq import PikaQ
+    new_pq = PikaQ(vals)
+    assert new_pq.peek() == result
+
+
+def test_peeking_empty_reveals_error(new_empty_priority_q):
+    """Test that peek() on empty Priority Q reveals an error."""
+    assert new_empty_priority_q.peek() == "Nothing, Priority Q is empty!"
