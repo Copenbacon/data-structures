55fa7df6f7d88da60a6c1e051856847f586d33ea
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-02 14:39:09 -0800

    fix line length too long linter issues

--- src/graphimp.py
+++ src/graphimp.py
@@ -5,15 +5,31 @@
 class Graph(object):
     u"""Class definition for the Graph Object.
 
-    g.nodes(): return a list of all nodes in the graph
-    g.edges(): return a list of all edges in the graph
-    g.add_node(n): adds a new node ‘n’ to the graph
-    g.add_edge(n1, n2): adds a new edge to the graph connecting ‘n1’ and ‘n2’, if either n1 or n2 are not already present in the graph, they should be added.
-    g.del_node(n): deletes the node ‘n’ from the graph, raises an error if no such node exists
-    g.del_edge(n1, n2): deletes the edge connecting ‘n1’ and ‘n2’ from the graph, raises an error if no such edge exists
-    g.has_node(n): True if node ‘n’ is contained in the graph, False if not.
-    g.neighbors(n): returns the list of all nodes connected to ‘n’ by edges, raises an error if n is not in g
-    g.adjacent(n1, n2): returns True if there is an edge connecting n1 and n2, False if not, raises an error if either of the supplied nodes are not in g
+    g.nodes():
+        return a list of all nodes in the graph
+    g.edges():
+        return a list of all edges in the graph
+    g.add_node(n):
+        adds a new node ‘n’ to the graph
+    g.add_edge(n1, n2):
+        adds a new edge to the graph connecting ‘n1’ and ‘n2’,
+        if either n1 or n2 are not already present in the graph,
+        they should be added.
+    g.del_node(n):
+        deletes the node ‘n’ from the graph,
+        raises an error if no such node exists
+    g.del_edge(n1, n2):
+        deletes the edge connecting ‘n1’ and ‘n2’ from the graph,
+        raises an error if no such edge exists
+    g.has_node(n):
+        True if node ‘n’ is contained in the graph, False if not.
+    g.neighbors(n):
+        returns the list of all nodes connected to ‘n’ by edges,
+        raises an error if n is not in g
+    g.adjacent(n1, n2):
+        returns True if there is an edge connecting n1 and n2,
+        False if not, raises an error if either of the supplied nodes
+        are not in g
     """
 
     def __init__(self):
@@ -38,7 +54,11 @@ class Graph(object):
         self.nodetionary.setdefault(n, [])
 
     def add_edge(self, n1, n2):
-        u"""Add a new edge to the graph connecting ‘n1’ and ‘n2’, if either n1 or n2 are not already present in the graph, they should be added."""
+        u"""Add a new edge to the graph connecting ‘n1’ and ‘n2’.
+
+        If either n1 or n2 are not already present in the graph,
+        they should be added.
+        """
         try:
             self.nodetionary[n1].append(n2)
         except KeyError:
@@ -49,7 +69,10 @@ class Graph(object):
             self.nodetionary.setdefault(n2, [n1])
 
     def del_node(self, n):
-        u"""Delete the node ‘n’ from the graph, raises an error if no such node exists."""
+        u"""Delete the node ‘n’ from the graph.
+
+        Raises an error if no such node exists.
+        """
         try:
             del self.nodetionary[n]
             for key in self.nodetionary:
@@ -59,7 +82,10 @@ class Graph(object):
             raise KeyError("Can't delete a node that doesn't exist.")
 
     def del_edge(self, n1, n2):
-        u"""Delete the edge connecting n1 and n2 from the graph, raises an error if no such edge exists."""
+        u"""Delete the edge connecting n1 and n2 from the graph.
+
+        Raises an error if no such edge exists.
+        """
         if self.has_node(n1) is False or self.has_node(n2) is False:
             raise KeyError("No such node in graph.")
         try:
@@ -94,9 +120,11 @@ class Graph(object):
                 return True
             return False
         else:
-            raise KeyError("One of the supplied nodes is not in the graph. Here is a current list of nodes: " + str(self.nodes()))
+            raise KeyError("One of the supplied nodes is not in the graph." +
+                           "Here is a current list of nodes: " +
+                           str(self.nodes()))
 
-    def depth_first_traversal(self, start):
+    def breadth_first_traversal(self, start):
         """Perform a full depth-first traversal of the graph beginning at start.
 
         Return the full visited path when traversal is complete.
@@ -110,3 +138,9 @@ class Graph(object):
                 visited.append(vertex)
                 stack.extend(self.nodetionary[vertex])
         return visited
+
+    def depth_first_traversal(self, start):
+        """Perform a full breadth-first traversal of the graph, beginning at start.
+
+        Return the full visited path when traversal is complete.
+        """
