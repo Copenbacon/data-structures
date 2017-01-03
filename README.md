728baa6e344732b19c395e4e171ded9b56f42e92
copenbacon <sclary50@gmail.com>
2017-01-02 21:36:48 -0800

    Update graph

--- README.md
+++ README.md
@@ -1,5 +1,59 @@
 # data-structures
 
+##Jan 2, 2016
+###Graph
+
+---------- coverage: platform linux2, python 2.7.12-final-0 ----------
+Name                      Stmts   Miss  Cover   Missing
+-------------------------------------------------------
+src/binheap.py               47      0   100%
+src/deque.py                 33      0   100%
+src/dll.py                   75      0   100%
+src/graphimp.py              48      0   100%
+src/linked_list.py           59      0   100%
+src/pq.py                    41      2    95%   34-35
+src/queue_.py                18      0   100%
+src/stack.py                 15      0   100%
+src/test_binheap.py          34      0   100%
+src/test_deque.py            78      0   100%
+src/test_dll.py              71      0   100%
+src/test_graph.py            87      0   100%
+src/test_linked_list.py      75      0   100%
+src/test_pq.py               48      4    92%   53-57
+src/test_queue.py            56      0   100%
+src/test_stack.py            39      0   100%
+-------------------------------------------------------
+TOTAL                       824      6    99%
+
+
+======================================================================= 223 passed in 0.74 seconds
+
+----------- coverage: platform linux, python 3.5.2-final-0 -----------
+Name                      Stmts   Miss  Cover   Missing
+-------------------------------------------------------
+src/binheap.py               47      0   100%
+src/deque.py                 33      0   100%
+src/dll.py                   75      0   100%
+src/graphimp.py              48      0   100%
+src/linked_list.py           59      0   100%
+src/pq.py                    41      0   100%
+src/queue_.py                18      0   100%
+src/stack.py                 15      0   100%
+src/test_binheap.py          34      0   100%
+src/test_deque.py            78      0   100%
+src/test_dll.py              71      0   100%
+src/test_graph.py            87      0   100%
+src/test_linked_list.py      75      0   100%
+src/test_pq.py               48      0   100%
+src/test_queue.py            56      0   100%
+src/test_stack.py            39      0   100%
+-------------------------------------------------------
+TOTAL                       824      0   100%
+
+
+======================================================================= 224 passed in 0.80 seconds
+
+
 
 ##Dec 28, 2016##
 ###Graph###
--- src/graphimp.py
+++ src/graphimp.py
@@ -5,15 +5,31 @@
 class Graph(object):
     u"""Class definition for the Graph Object.
 
-    g.nodes(): return a list of all nodes in the graph
-    g.edges(): return a list of all edges in the graph
-    g.add_node(n): adds a new node ‘n’ to the graph
-    g.add_edge(n1, n2): adds a new edge to the graph connecting ‘n1’ and ‘n2’, if either n1 or n2 are not already present in the graph, they should be added.
-    g.del_node(n): deletes the node ‘n’ from the graph, raises an error if no such node exists
-    g.del_edge(n1, n2): deletes the edge connecting ‘n1’ and ‘n2’ from the graph, raises an error if no such edge exists
-    g.has_node(n): True if node ‘n’ is contained in the graph, False if not.
-    g.neighbors(n): returns the list of all nodes connected to ‘n’ by edges, raises an error if n is not in g
-    g.adjacent(n1, n2): returns True if there is an edge connecting n1 and n2, False if not, raises an error if either of the supplied nodes are not in g
+    g.nodes():
+        return a list of all nodes in the graph
+    g.edges():
+        return a list of all edges in the graph
+    g.add_node(n):
+        adds a new node ‘n’ to the graph
+    g.add_edge(n1, n2):
+        adds a new edge to the graph connecting ‘n1’ and ‘n2’,
+        if either n1 or n2 are not already present in the graph,
+        they should be added.
+    g.del_node(n):
+        deletes the node ‘n’ from the graph,
+        raises an error if no such node exists
+    g.del_edge(n1, n2):
+        deletes the edge connecting ‘n1’ and ‘n2’ from the graph,
+        raises an error if no such edge exists
+    g.has_node(n):
+        True if node ‘n’ is contained in the graph, False if not.
+    g.neighbors(n):
+        returns the list of all nodes connected to ‘n’ by edges,
+        raises an error if n is not in g
+    g.adjacent(n1, n2):
+        returns True if there is an edge connecting n1 and n2,
+        False if not, raises an error if either of the supplied nodes
+        are not in g
     """
 
     def __init__(self):
@@ -38,18 +54,19 @@ class Graph(object):
         self.nodetionary.setdefault(n, [])
 
     def add_edge(self, n1, n2):
-        u"""Add a new edge to the graph connecting ‘n1’ and ‘n2’, if either n1 or n2 are not already present in the graph, they should be added."""
-        try:
-            self.nodetionary[n1].append(n2)
-        except KeyError:
-            self.nodetionary.setdefault(n1, [n2])
-        try:
-            self.nodetionary[n2].append(n1)
-        except KeyError:
-            self.nodetionary.setdefault(n2, [n1])
+        u"""Add a new edge to the graph connecting ‘n1’ and ‘n2’.
+
+        If either n1 or n2 are not already present in the graph,
+        they should be added.
+        """
+        self.nodetionary.setdefault(n1, []).append(n2)
+        self.nodetionary.setdefault(n2, [])
 
     def del_node(self, n):
-        u"""Delete the node ‘n’ from the graph, raises an error if no such node exists."""
+        u"""Delete the node ‘n’ from the graph.
+
+        Raises an error if no such node exists.
+        """
         try:
             del self.nodetionary[n]
             for key in self.nodetionary:
@@ -59,12 +76,14 @@ class Graph(object):
             raise KeyError("Can't delete a node that doesn't exist.")
 
     def del_edge(self, n1, n2):
-        u"""Delete the edge connecting n1 and n2 from the graph, raises an error if no such edge exists."""
-        if self.has_node(n1) is False or self.has_node(n2) is False:
+        u"""Delete the edge connecting n1 and n2 from the graph.
+
+        Raises an error if no such edge exists.
+        """
+        if not self.has_node(n1) or not self.has_node(n2):
             raise KeyError("No such node in graph.")
         try:
             self.nodetionary[n1].remove(n2)
-            self.nodetionary[n2].remove(n1)
         except ValueError:
             raise ValueError("No such edge exists.")
 
@@ -75,17 +94,26 @@ class Graph(object):
         return False
 
     def neighbors(self, n):
-        u"""Return the list of all nodes connected to ‘n’ by edges, raises an error if n is not in graph."""
+        u"""Return the list of all nodes connected to ‘n’ by edges.
+
+        Raises an error if n is not in graph.
+        """
         try:
             return list(self.nodetionary[n])
         except KeyError:
             raise KeyError("Node {} is not in the graph".format(n))
 
     def adjacent(self, n1, n2):
-        u"""Return True if there is an edge connecting n1 and n2, False if not, raises an error if either of the supplied nodes are not in graph."""
+        u"""Return True if there is an edge connecting n1 and n2, False if not.
+
+        Raises an error if either of the supplied nodes are not in graph.
+        """
         if n1 in self.nodetionary and n2 in self.nodetionary:
-            if n1 in self.nodetionary[n2] and n2 in self.nodetionary[n1]:
+            if n2 in self.nodetionary[n1]:
                 return True
             return False
         else:
-            raise KeyError("One of the supplied nodes is not in the graph. Here is a current list of nodes:" + str(self.nodes()))
+            raise KeyError("One of the supplied nodes is not in the graph." +
+                           "Here is a current list of nodes: " +
+                           str(self.nodes()))
+
